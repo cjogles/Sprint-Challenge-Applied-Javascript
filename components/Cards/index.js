6f4afc5b531ff05f4e6960d6422ea505e4622640
cjogles <jackson.ogles28@gmail.com>
2019-10-04 11:17:43 -0600

    created card component to return one card completely. Still need to work on returning all cards and all tabs

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,12 +17,39 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function Card(obj) {
+    let newCard = document.createElement("div");
+    let headLine = document.createElement("div");
+    let author = document.createElement("div");
+    let imgContainer = document.createElement("div");
+    let pic = document.createElement("img");
+    let byAuthorName = document.createElement("span");
+    // structure content elements
+    newCard.appendChild(headLine);
+    newCard.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(pic);
+    author.appendChild(byAuthorName);
+    // connect classes for styling
+    newCard.classList.add("card");
+    headLine.classList.add("headline");
+    author.classList.add("author");
+    imgContainer.classList.add("img-container");
+    // populate elements with content
+    pic.src = obj.data.articles.bootstrap[0].authorPhoto;
+    headLine.textContent = obj.data.articles.bootstrap[0].headline;
+    byAuthorName.textContent = obj.data.articles.bootstrap[0].authorName;
+    //return card
+    return newCard;
+}
+let card = document.querySelector(".cards-container");
 
 axios.get("https://lambda-times-backend.herokuapp.com/articles")
 .then(function (response) {
     // handle success
     console.log(response);
-    
+    let newCard = Card(response);
+    card.appendChild(newCard);
   })
   .catch(function (error) {
     // handle error
